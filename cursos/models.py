aceb37a68691b5a4b4e247a122e6773cf14b29ea
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2026-04-07 21:31:41 -0300

    creado modelo, hechas migraciones y mostrar items del modelo

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    duracion = models.IntegerField()
+    precio = models.IntegerField()
+    nivel = models.CharField(max_length=100)
+    modalidad = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Soy el curso {self.nombre}'
--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Curso
 
 def inicio(request):
     return render(request, 'cursos/index.html')
 
 def cursos(request):
-    return render(request, 'cursos/cursos.html')
+    cursos = Curso.objects.all()
+    return render(request, 'cursos/cursos.html', {'cursos': cursos})
