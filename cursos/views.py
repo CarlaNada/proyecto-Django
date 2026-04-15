df5fe2ca42255522aa48c8587963f5b9bc306d2a
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2026-04-14 21:18:44 -0300

    se agrego order by para mostrar primero los cursos nuevos

--- cursos/views.py
+++ cursos/views.py
@@ -5,5 +5,5 @@ def inicio(request):
     return render(request, 'cursos/index.html')
 
 def cursos(request):
-    cursos = Curso.objects.all()
+    cursos = Curso.objects.all().order_by("-id")
     return render(request, 'cursos/cursos.html', {'cursos': cursos})
