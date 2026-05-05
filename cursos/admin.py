9649a18274ee10816a7d53202961d9ea7a86ad00
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-05-05 12:13:18 -0300

    Nueva tabla con relacion

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Curso
+from .models import Curso, Modalidad
 # Register your models here.
 
 admin.site.register(Curso)
+admin.site.register(Modalidad)
--- cursos/models.py
+++ cursos/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+#from django.contrib.auth.model import User --> para acceder al modelo user de django
+
+class Modalidad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     duracion = models.IntegerField()
     precio = models.IntegerField()
     nivel = models.CharField(max_length=100)
-    modalidad = models.CharField(max_length=100)
     activo = models.BooleanField(default=True)
+    modalidad = models.ForeignKey(Modalidad, on_delete=models.CASCADE, related_name="modalidad", default=None, null=True, blank=True)
 
     def __str__(self):
         return f'Soy el curso {self.nombre}'
